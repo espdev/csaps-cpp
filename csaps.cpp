d09104b7c9d9cf4f02e51939ece3bbf0332aca94
Eugene Prilepin <esp.home@gmail.com>
2017-06-25 00:47:51 +0300

    Add skeleton of code and test

--- csaps.cpp
+++ csaps.cpp
@@ -1,6 +1,78 @@
+#include <iostream>
+
 #include "csaps.h"
 
 namespace csaps
 {
 
+UnivariateCubicSmoothingSpline::UnivariateCubicSmoothingSpline(const DoubleVector &xdata, const DoubleVector &ydata)
+  : UnivariateCubicSmoothingSpline(xdata, ydata, DoubleVector(), -1.0)
+{
 }
+
+UnivariateCubicSmoothingSpline::UnivariateCubicSmoothingSpline(const DoubleVector &xdata, const DoubleVector &ydata, const DoubleVector &weights)
+  : UnivariateCubicSmoothingSpline(xdata, ydata, weights, -1.0)
+{
+}
+
+UnivariateCubicSmoothingSpline::UnivariateCubicSmoothingSpline(const DoubleVector &xdata, const DoubleVector &ydata, double smooth)
+  : UnivariateCubicSmoothingSpline(xdata, ydata, DoubleVector(), smooth)
+{
+}
+
+UnivariateCubicSmoothingSpline::UnivariateCubicSmoothingSpline(const DoubleVector &xdata, const DoubleVector &ydata, const DoubleVector &weights, double smooth)
+  : m_xdata(xdata)
+  , m_ydata(ydata)
+  , m_weights(weights)
+  , m_smooth(smooth)
+{
+  if (m_xdata.size() < 2) {
+    throw std::exception("There must be at least 2 data points");
+  }
+
+  if (m_weights.size() == 0) {
+    m_weights = Eigen::VectorXd::Constant(m_xdata.size(), 1.0);
+  }
+
+  if (m_smooth > 1.0) {
+    throw std::exception("Smoothing parameter must be less than or equal 1.0");
+  }
+
+  if (m_xdata.size() != m_ydata.size() || m_xdata.size() != m_weights.size()) {
+    throw std::exception("Lenghts of the input data vectors are not equal");
+  }
+
+  MakeSpline();
+}
+
+DoubleVector UnivariateCubicSmoothingSpline::operator()(const DoubleVector &xidata)
+{
+  if (xidata.size() < 2) {
+    throw std::exception("There must be at least 2 data points");
+  }
+
+  return Evaluate(xidata);
+}
+
+DoubleVector UnivariateCubicSmoothingSpline::operator()(size_t pcount)
+{
+  if (pcount < 2) {
+    throw std::exception("There must be at least 2 data points");
+  }
+
+  DoubleVector xidata = DoubleVector::LinSpaced(pcount, m_xdata(0), m_xdata(m_xdata.size()-1));
+
+  return Evaluate(xidata);
+}
+
+void UnivariateCubicSmoothingSpline::MakeSpline()
+{
+  size_t pcount = m_xdata.size();
+}
+
+DoubleVector UnivariateCubicSmoothingSpline::Evaluate(const DoubleVector & xidata)
+{
+  return DoubleVector();
+}
+
+} // namespace csaps
--- csaps.h
+++ csaps.h
@@ -2,9 +2,40 @@
 #ifndef CSAPS_H
 #define CSAPS_H
 
+#include <vector>
+
+#include <Eigen/Dense>
+
+
 namespace csaps
 {
 
-}
+typedef Eigen::VectorXd DoubleVector;
+
+
+class UnivariateCubicSmoothingSpline
+{
+public:
+  UnivariateCubicSmoothingSpline(const DoubleVector &xdata, const DoubleVector &ydata);
+  UnivariateCubicSmoothingSpline(const DoubleVector &xdata, const DoubleVector &ydata, const DoubleVector &weights);
+  UnivariateCubicSmoothingSpline(const DoubleVector &xdata, const DoubleVector &ydata, double smooth);
+  UnivariateCubicSmoothingSpline(const DoubleVector &xdata, const DoubleVector &ydata, const DoubleVector &weights, double smooth);
+
+  DoubleVector operator()(const DoubleVector &xidata);
+  DoubleVector operator()(size_t pcount);
+
+protected:
+  void MakeSpline();
+  DoubleVector Evaluate(const DoubleVector &xidata);
+
+protected:
+  DoubleVector m_xdata;
+  DoubleVector m_ydata;
+  DoubleVector m_weights;
+
+  double m_smooth;
+};
+
+} // namespace csaps
 
 #endif // CSAPS_H
